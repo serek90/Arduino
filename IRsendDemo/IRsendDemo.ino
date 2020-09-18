026bdb6c4265c3c2ad52d445a83be1e5fd07330c
serek90 <43785920+serek90@users.noreply.github.com>
2020-09-18 18:58:21 +0200

    Update IRsendDemo.ino

--- IRsendDemo/IRsendDemo.ino
+++ IRsendDemo/IRsendDemo.ino
@@ -3,29 +3,34 @@
 IRsend irsend;
 
 void setup()
-{ Serial.begin(9600);
+{ 
+	Serial.begin(9600);
 }
 
 void loop() {
 
-while (Serial.available() > 0) {
-int signal = Serial.parseInt();
-   irsend.sendRC5(000, 12);
-   delay(20);
-   int a = signal/10;
-   int b = signal%10;
-   for( int i=0; i< a; i++){
-   irsend.sendRC5(010, 12);
-   delay(20);
-   }
-   for( int i=0; i< b; i++){
-		irsend.sendRC5(001, 12);
-    delay(20);
-   }
-		
-  
-  
- } 
+    while (Serial.available() > 0) 
+    {
 
+	int signal = Serial.parseInt();
+	irsend.sendRC5(000, 12);
+   	
+	delay(20);
+   
+	int a = signal/10;
+   	int b = signal%10;
+   
+	for( int i = 0; i < a; i++)
+	{
+            irsend.sendRC5(010, 12);
+            delay(20);
+        }
+	    
+        for( int i=0; i< b; i++)
+	{
+	    irsend.sendRC5(001, 12);
+            delay(20);
+        }
+    } 
 }
 
